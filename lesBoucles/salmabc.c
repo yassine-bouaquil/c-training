4479b37f3409eb5454b9b48a88b7bf145be835e4
Elgadisalma <elgadi.salma0@gmail.com>
2023-09-14 16:03:12 +0100

    salma

---
+++ lesBoucles/salmabc.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main() {
+int i;
+ printf("MA PUNITION \n");
+for(i=1;i<50;i++){
+printf("%d salma elgadi \n",i);
+}
+printf(".\n");
+printf("je promis de ne jamais répèter \n");
+    int j = 50;
+    while (j <= 100) {
+        printf("%d je promis de ne jamais répèter \n ", j);
+        j++;
+    }
+    printf("\n");
+
+    return 0;
+}
