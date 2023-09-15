d387458668d4cbc27c2ad33e70ee606f45068340
WAFAALAAOUINA <laaouinawafaa96@gmail.com>
2023-09-15 16:44:25 +0100

    wafaa

---
+++ fonctions/wafaa.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+int min( int a, int b){
+if (a<=b)
+return a;
+else
+return b;
+ }
+ int max( int a, int b) {
+if(a>=b)
+return a;
+#include <stdio.h>
+#include <stdlib.h>
+int min( int a, int b){
+if (a<=b)
+return a;
+else
+return b;
+ }
+ int max( int a, int b) {
+if(a>=b)
+return a;
+else
+return b;
+}
+
+int  main()
+{
+int a,b,c,d;
+int minimum;
+int maximum;
+printf("saisir le 1er nombre\n");
+scanf("%d",&a);
+printf("saisir le 2eme nombre\n");
+scanf("%d",&b);
+printf("saisir le 3eme nombre\n");
+scanf("%d",&c);
+printf("saisir le 4eme nombre\n");
+scanf("%d",&d);
+
