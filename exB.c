ad9dd12052e5f3bfffddee8cd1049273197a2b7d
WAFAALAAOUINA <laaouinawafaa96@gmail.com>
2023-09-19 13:47:15 +0100

    wafaa

---
+++ exB.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+struct Point {
+    double x;
+    double y;
+};
+
+double calculerDistance(struct Point p1, struct Point p2) {
+    return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+int main() {
+    
+    struct Point point1, point2;
+
+    printf("Entrez les coordonnées du premier point (x1 y1) : ");
+    scanf("%lf %lf", &point1.x, &point1.y);
+
+    printf("Entrez les coordonnées du deuxième point (x2 y2) : ");
+    scanf("%lf %lf", &point2.x, &point2.y);
+
+ 
+    double distance = calculerDistance(point1, point2);
+
+
+    printf("La distance entre les points est : %lf\n", distance);
+
+    return 0;
+}
