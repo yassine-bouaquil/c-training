e48333cd5b2c97956223ed294a76f99fcbdd7549
medsar21 <mohamed12sar@gmail.com>
2023-09-19 16:02:54 +0100

    mohamed sarghini chalenge3

--- chalenge/chalenge3.c
+++ chalenge/chalenge3.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
+
+struct Employee {
+    char employeeName[50];
+    float hourlySalary;
+    float hoursWorked;
+};
+
 int main() {
-   
-    int age;
+     struct Employee employee;
+    printf("Enter the employee's name: ");
+    scanf("%s", employee.employeeName);
 
-    
-    printf("Entrez l'âge : ");
-    scanf("%d", &age);
+    printf("Enter the hourly salary: ");
+    scanf("%f", &employee.hourlySalary);
 
+    printf("Enter the hours worked: ");
+    scanf("%f", &employee.hoursWorked);
     
-    if (age >= 18) {
-        printf("The person is an adult.\n");
-    } else {
-        printf("The person is a minor.\n");
-    }
+   float totalSalary = employee.hourlySalary * employee.hoursWorked;
+
+    printf("Employee's name: %s\n", employee.employeeName);
+    printf("Total salary: %.2f\n", totalSalary);
 
     return 0;
 }
+
